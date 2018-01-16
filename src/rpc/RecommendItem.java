5dde9aa638d9bb4bf497e5175acd67908bcfad78
Bo Li <bli2@andrew.cmu.edu>
2018-01-15 20:11:31 -0600

    Add verification in existing servlets.

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONArray;
 
@@ -36,7 +37,13 @@ public class RecommendItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String userId = request.getParameter("user_id");
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		String userId = session.getAttribute("user").toString();
+//		String userId = request.getParameter("user_id");
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		Recommendation recommendation = new GeoRecommendation();
